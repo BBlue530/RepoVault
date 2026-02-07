62e495b959d2623e7460d30351e548783762bf65
BBlue530 <davidlundegard@gmail.com>
2026-02-07 13:31:14 +0100

    feat: Added ip whitelist for github actions

--- src/lambda_handler.py
+++ src/lambda_handler.py
@@ -9,32 +9,43 @@ from urllib.parse import unquote
 from s3_handling import backup_repos_s3_bucket, cleanup_old_s3_backups
 from secret_manager import read_secret_from_secret_manager
 from helpers import sterilize_output
+from ip_whitelist import verify_ip_whitelist
 from variables import *
 
 def lambda_backup_repository(event, context):
-    try:
-        body = event.get("body")
-        if body:
-            event = json.loads(body)
+    body = event.get("body")
+    if body:
+        event_body = json.loads(body)
 
-        received_api_key = unquote(event.get("api_key", ""))
-        repo_url = unquote(event.get("repo_url", ""))
+    received_api_key = unquote(event_body.get("api_key", ""))
+    repo_url = unquote(event_body.get("repo_url", ""))
 
-        API_KEY = read_secret_from_secret_manager(api_key_secret_name, secret_name)
+    client_ip = event.get("requestContext", {}).get("http", {}).get("sourceIp")
+    print(f"[+] Client ip: [{client_ip}]")
 
-        hashed_received_api_key = hashlib.sha256(received_api_key.encode("utf-8")).hexdigest()
-        HASHED_API_KEY = hashlib.sha256(API_KEY.encode("utf-8")).hexdigest()
+    if not verify_ip_whitelist(client_ip):
+        print("[!] Client ip not found in whitelist")
+        return {
+            "statusCode": 403,
+            "body": json.dumps({"error": "IP not allowed"})
+            }
+    
+    API_KEY = read_secret_from_secret_manager(api_key_secret_name, secret_name)
 
-        print(f"[+] Hashed received API key: [{hashed_received_api_key}]")
-        print(f"[+] Hashed API key:          [{HASHED_API_KEY}]")
+    hashed_received_api_key = hashlib.sha256(received_api_key.encode("utf-8")).hexdigest()
+    HASHED_API_KEY = hashlib.sha256(API_KEY.encode("utf-8")).hexdigest()
 
-        if hashed_received_api_key != HASHED_API_KEY:
-            print("[!] API key mismatch")
-            return {
-                "statusCode": 400,
-                "body": "api key mismatch"
-                }
+    print(f"[+] Hashed received API key: [{hashed_received_api_key}]")
+    print(f"[+] Hashed API key: [{HASHED_API_KEY}]")
+
+    if hashed_received_api_key != HASHED_API_KEY:
+        print("[!] API key mismatch")
+        return {
+            "statusCode": 400,
+            "body": json.dumps({"error": "api key mismatch"})
+            }
         
+    try:
         pat = read_secret_from_secret_manager(github_pat_secret_name, secret_name)
         
         repo_git = repo_url.split("/")[-1]
